f96b24f022a0845f8e3b1f7cb91389f4c5da0cea
hoannguyen02 <hoan@rubrikkgroup.com>
2020-04-08 09:25:20 +0700

    Test modal with form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import Modal from './modal';
+import TestModalWithForm from './test-modal-with-form';
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
@@ -13,26 +13,11 @@ function App() {
     setOpenModal(false);
   };
 
-  const handleSubmit = (values) => {
-    console.log(values);
-  };
-
   return (
     <div className="App">
       <h2>Welcome to modal based on bootstrap for my company project</h2>
       <button onClick={handleOpenModal}>Open</button>
-      <Modal
-        open={openModal}
-        onClose={handleCloseModal}
-        onSubmit={handleSubmit}
-      >
-        There was some specs such as styles without flex, state full component
-        with class
-        <div>
-          <input type="email" />
-          <button type="submit">Submit</button>
-        </div>
-      </Modal>
+      <TestModalWithForm open={openModal} onClose={handleCloseModal} />
     </div>
   );
 }
---
+++ src/test-modal-with-form/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Modal from '../modal';
+
+export default class TestModalWithForm extends React.Component {
+  state = {
+    email: '',
+  };
+
+  render() {
+    const { open, onClose } = this.props;
+    const { email } = this.state;
+
+    const handleSubmit = () => {
+      console.log(this.state.email);
+      email && this.setState({ email: '' });
+      onClose && onClose();
+    };
+
+    const handleEmailChange = (event) => {
+      const { value } = event.target;
+      this.setState({ email: value });
+    };
+
+    const handleClose = () => {
+      email && this.setState({ email: '' });
+      onClose && onClose();
+    };
+
+    return (
+      <Modal open={open} onClose={handleClose} onSubmit={handleSubmit}>
+        There was some specs such as styles without flex, state full component
+        with class
+        <div>
+          <input type="email" value={email} onChange={handleEmailChange} />
+          <button type="submit">Submit</button>
+        </div>
+      </Modal>
+    );
+  }
+}
